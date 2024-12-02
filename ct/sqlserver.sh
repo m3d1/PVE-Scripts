378ba77ef6a90fa3dc501c889e56a41a4cf14766
m3d1 <basri.mehdi@gmail.com>
2024-12-02 18:03:33 +0100

    change path of build.func

--- ct/sqlserver.sh
+++ ct/sqlserver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/m3d1/ProxmoxVE/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/m3d1/PVE-Scripts/refs/heads/main/misc/build.func)
 # Copyright (c) 2024 M3d1
 # Author: M3d1 (tteckster)
 # License: MIT
