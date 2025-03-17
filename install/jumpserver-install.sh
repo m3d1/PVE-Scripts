0b28d787cd10b82b77bf8797bb7e8bbc24984db1
m3d1 <basri.mehdi@gmail.com>
2025-03-17 14:59:37 +0000

    Update jumpserver-install.sh

--- install/jumpserver-install.sh
+++ install/jumpserver-install.sh
@@ -32,12 +32,12 @@ function check_distro()
 {
 apt install lsb-release -y
 # Allowed Distro Versions
-VERSION=v4.7.0
+VERSION=4.7.0
 DOWNLOAD_URL=https://github.com
 DEBIAN_VERSIONS=("11" "12")
 UBUNTU_VERSIONS=("22.04" "24.04")
 DISTRO=$(lsb_release -is)
-VERSION=$(lsb_release -rs)
+LSB_VERSION=$(lsb_release -rs)
 
 
 if [[ "${OS}" == 'Darwin' ]]; then
@@ -48,10 +48,10 @@ fi
 
 # Debian Distribution Check
 if [ "$DISTRO" == "Debian" ]; then
-        if [[ " ${DEBIAN_VERSIONS[*]} " == *" $VERSION "* ]]; then
-                msg_info "the OS: $DISTRO $VERSION is compatible."
+        if [[ " ${DEBIAN_VERSIONS[*]} " == *" $LSB_VERSION "* ]]; then
+                msg_info "the OS: $DISTRO $LSB_VERSION is compatible."
         else
-                msg_error "This OS $DISTRO $VERSION is not compatible"
+                msg_error "This OS $DISTRO $LSB_VERSION is not compatible"
                 msg_error "Would you like to force the installation?"
                 msg_info "Would you like to continue? [y/N]"
                 read response
@@ -68,10 +68,10 @@ if [ "$DISTRO" == "Debian" ]; then
 
 # Ubuntu Distribution Check
 elif [ "$DISTRO" == "Ubuntu" ]; then
-        if [[ " ${UBUNTU_VERSIONS[*]} " == *" $VERSION "* ]]; then
-                msg_info "the OS: $DISTRO $VERSION is compatible."
+        if [[ " ${UBUNTU_VERSIONS[*]} " == *" $LSB_VERSION "* ]]; then
+                msg_info "the OS: $DISTRO $LSB_VERSION is compatible."
         else
-                msg_error "This OS $DISTRO $VERSION is not compatible"
+                msg_error "This OS $DISTRO $LSB_VERSION is not compatible"
                 msg_error "Would you like to force the installation?"
                 msg_info "Would you like to continue? [y/N]"
                 read response
